04cf1f0bf0a5e378f69bc3d6557996479c6a4bdf
RedMurder07 <163778466+RedMurder07@users.noreply.github.com>
2024-03-25 00:34:25 +0000

    Update score.js

--- js/score.js
+++ js/score.js
@@ -25,7 +25,7 @@ export function score(rank, percent, minPercent) {
   */
   var listnum = 71;
   // New formula
-  let score = (-24.9975 * Math.pow(((rank * (150 / listnum)) - (150 / listnum)), 0.4) + 200) *
+  let score = (-24.9975 * Math.pow(((rank * (150 / listnum)) - (150 / listnum)), 0.4) + 250) *
     ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
   score = Math.max(0, score);

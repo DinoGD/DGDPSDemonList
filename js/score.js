58fc9eddec22481c870e8a4c60e73e9e5ef014ed
DinoGD <141871495+DinoGD@users.noreply.github.com>
2024-03-25 00:55:39 +0000

    Update score.js

--- js/score.js
+++ js/score.js
@@ -1,55 +1,50 @@
 /**
  * Numbers of decimal digits to round to
  */
-const scale = 3;
+ const scale = 2;
 
-/**
- * Calculate the score awarded when having a certain percentage on a list level
- * @param {Number} rank Position on the list
- * @param {Number} percent Percentage of completion
- * @param {Number} minPercent Minimum percentage required
- * @returns {Number}
- */
-export function score(rank, percent, minPercent) {
-  if (rank > 150) {
-    return 0;
-  }
-  if (rank > 75 && percent < 100) {
-    return 0;
-  }
-
-  // Old formula
-  /*
-  let score = (100 / Math.sqrt((rank - 1) / 50 + 0.444444) - 50) *
-      ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
+ /**
+  * Calculate the score awarded when having a certain percentage on a list level
+  * @param {Number} rank Position on the list
+  * @param {Number} percent Percentage of completion
+  * @param {Number} minPercent Minimum percentage required
+  * @returns {Number}
   */
-  var listnum = 76;
-  // New formula
-  let score = (-24.9975 * Math.pow(((rank * (150 / listnum)) - (150 / listnum)), 0.4) + 250) *
-    ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
-
-  score = Math.max(0, score);
-
-if (percent != 100) { score = (score * Math.pow(5, ((percent - minPercent) / (100 - minPercent)))) / 10; }
-    return Number(score.toFixed(2));
-
-
-  return Math.max(round(score), 0);
-}
-
-export function round(num) {
-  if (!('' + num).includes('e')) {
-    return +(Math.round(num + 'e+' + scale) + 'e-' + scale);
-  } else {
-    var arr = ('' + num).split('e');
-    var sig = '';
-    if (+arr[1] + scale > 0) {
-      sig = '+';
+ export function score(rank, percent, minPercent) {
+     if (rank === null || rank > 150) {
+         return 0;
+     }
+     if (rank > 75 && percent < 100) {
+        return 0;
     }
-    return +(
-      Math.round(+arr[0] + 'e' + sig + (+arr[1] + scale)) +
-      'e-' +
-      scale
-    );
-  }
-}
+     let maximum_points = 250; //change this to change points of top 1
+     let score = ((140 * maximum_points + 7000) / Math.sqrt(3157 * (rank - 1) + 19600) - 50) *
+         ((percent - (minPercent - 1)) / (percent - (minPercent - 1)));
+ 
+     score = Math.max(0, score);
+     if (isNaN(score)) {
+         score = 0;
+     }
+ 
+     if (percent != 100) { score = (score * Math.pow(5, ((percent - minPercent) / (100 - minPercent)))) / 10; }
+    return Number(score.toFixed(2));
+ 
+     return round(score);
+ }
+ 
+ export function round(num) {
+     if (!('' + num).includes('e')) {
+         return +(Math.round(num + 'e+' + scale) + 'e-' + scale);
+     } else {
+         var arr = ('' + num).split('e');
+         var sig = '';
+         if (+arr[1] + scale > 0) {
+             sig = '+';
+         }
+         return +(
+             Math.round(+arr[0] + 'e' + sig + (+arr[1] + scale)) +
+             'e-' +
+             scale
+         );
+     }
+ }

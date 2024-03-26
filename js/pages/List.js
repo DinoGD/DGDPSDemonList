bf4e5efac555bc3a406e52cae755db09d65b4f0d
DinoGD <141871495+DinoGD@users.noreply.github.com>
2024-03-26 16:19:21 +0000

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -64,7 +64,7 @@ export default {
                     <p v-if="selected + 1 <= 75"><strong>{{ level.percentToQualify }}%</strong> or better to qualify</p>
                     <p v-else-if="selected +1 <= 150"><strong>100%</strong> to qualify</p>
                     <p v-else>This level does not accept new records.</p>
-                    <table class="records style="background-color:grey;" ">
+                    <table class="records style="background-color:grey;">
                         <tr v-for="record in level.records" class="record">
                             <td class="percent">
                                 <p>{{ record.percent }}%</p>
